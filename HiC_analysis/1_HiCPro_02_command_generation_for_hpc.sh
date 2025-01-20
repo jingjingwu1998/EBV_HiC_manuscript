807468a0d0673edc915dae41dec02e465b21206c
Jingjing Wu <112022982+jingjingwu1998@users.noreply.github.com>
2025-01-20 17:23:44 -0500

    Update 1_HiCPro_02_command_generation_for_hpc.sh

--- HiC_analysis/1_HiCPro_02_command_generation_for_hpc.sh
+++ HiC_analysis/1_HiCPro_02_command_generation_for_hpc.sh
@@ -3,16 +3,20 @@
 # This is the script to create HiC-Pro commands for HPC systems
 
 # initial working directories
-export PROJECT_DIR="/share/lab_teng/xiangliu/hic_analysis/hicpro_mapping_2"
-export RAW_DATA="/share/lab_teng/data/sequencing/hic_bo/raw_data"
-export MAP_DATA="/share/lab_teng/xiangliu/hic_analysis/hicpro_mapping_2"
+# export PROJECT_DIR="/share/lab_teng/xiangliu/hic_analysis/hicpro_mapping_2"
+# export RAW_DATA="/share/lab_teng/data/sequencing/hic_bo/raw_data"
+# export MAP_DATA="/share/lab_teng/xiangliu/hic_analysis/hicpro_mapping_2"
+export PROJECT_DIR="/share/lab_teng/trainee/JingjingWu/EBV/hicpro_mapping_2"
+export RAW_DATA="/share/lab_teng/trainee/JingjingWu/EBV/hic_bo/raw_data"
+export MAP_DATA="/share/lab_teng/trainee/JingjingWu/EBV/hicpro_mapping_2"
 
 module load gcc/5.4.0
 
 cd $PROJECT_DIR
 
 # Step 1: HiC-Pro mapping command
-/share/lab_teng/data/software/HiC-Pro_3.1.0/bin/HiC-Pro -c config-hicpro.txt -i $RAW_DATA -o hicpro_mapping_2 -s mapping -s quality_checks -p
+# /share/lab_teng/data/software/HiC-Pro_3.1.0/bin/HiC-Pro -c config-hicpro.txt -i $RAW_DATA -o hicpro_mapping_2 -s mapping -s quality_checks -p
+/share/lab_teng/trainee/JingjingWu/EBV/HiC_performance/HiC-Pro/bin/HiC-Pro -c config-hicpro.txt -i $RAW_DATA -o hicpro_mapping_2 -s mapping -s quality_checks -p
 
 # Step 2: HiC-Pro proc_hic command
 /share/lab_teng/data/software/HiC-Pro_3.1.0/bin/HiC-Pro -c config-hicpro.txt -i ${MAP_DATA}/bowtie_results/bwt2 -o proc_hic_out -s proc_hic -s quality_checks -p
