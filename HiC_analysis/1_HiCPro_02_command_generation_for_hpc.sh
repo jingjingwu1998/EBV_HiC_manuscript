10e9f16d386512c89aada039de46689f558aec93
Jingjing Wu <112022982+jingjingwu1998@users.noreply.github.com>
2025-01-20 19:03:49 -0500

    Update 1_HiCPro_02_command_generation_for_hpc.sh

--- HiC_analysis/1_HiCPro_02_command_generation_for_hpc.sh
+++ HiC_analysis/1_HiCPro_02_command_generation_for_hpc.sh
@@ -17,13 +17,14 @@ cd $PROJECT_DIR
 
 # Step 1: HiC-Pro mapping command
 # /share/lab_teng/data/software/HiC-Pro_3.1.0/bin/HiC-Pro -c config-hicpro.txt -i $RAW_DATA -o hicpro_mapping_2 -s mapping -s quality_checks -p
-/share/lab_teng/trainee/JingjingWu/EBV/HiC_performance/HiC-Pro_3.1.0/bin/HiC-Pro -c config-hicpro.txt -i $RAW_DATA -o hicpro_mapping_2 -s mapping -s quality_checks -p
+/share/lab_teng/trainee/JingjingWu/EBV/HiC_performance/HiC-Pro_install_path/HiC-Pro_3.1.0/bin/HiC-Pro -c config-hicpro.txt -i $RAW_DATA -o hicpro_mapping_2 -s mapping -s quality_checks -p
+
 
 # Step 2: HiC-Pro proc_hic command
-/share/lab_teng/trainee/JingjingWu/EBV/HiC_performance/HiC-Pro_3.1.0/bin/HiC-Pro -c config-hicpro.txt -i ${MAP_DATA}/bowtie_results/bwt2 -o proc_hic_out -s proc_hic -s quality_checks -p
+/share/lab_teng/trainee/JingjingWu/EBV/HiC_performance/HiC-Pro_install_path/HiC-Pro_3.1.0/bin/HiC-Pro -c config-hicpro.txt -i ${MAP_DATA}/bowtie_results/bwt2 -o proc_hic_out -s proc_hic -s quality_checks -p
 
 # Step 3: HiC-Pro merge_persample command 
-/share/lab_teng/trainee/JingjingWu/EBV/HiC_performance/HiC-Pro_3.1.0/bin/HiC-Pro -i ${MAP_DATA}/proc_hic_out/hic_results/data -o ${MAP_DATA}/merge_validpairs -c ${MAP_DATA}/config-hicpro.txt -s merge_persample -p
+/share/lab_teng/trainee/JingjingWu/EBV/HiC_performance/HiC-Pro_install_path/HiC-Pro_3.1.0/bin/HiC-Pro -i ${MAP_DATA}/proc_hic_out/hic_results/data -o ${MAP_DATA}/merge_validpairs -c ${MAP_DATA}/config-hicpro.txt -s merge_persample -p
 
 # Step 4: HiC-Pro build_contact_maps and ice_norm commands 
-/share/lab_teng/trainee/JingjingWu/EBV/HiC_performance/HiC-Pro_3.1.0/bin/HiC-Pro -i ${MAP_DATA}/merge_validpairs/hic_results/data -o ${MAP_DATA}/contact_maps -c ${MAP_DATA}/config-hicpro.txt -s build_contact_maps -s ice_norm -p
+/share/lab_teng/trainee/JingjingWu/EBV/HiC_performance/HiC-Pro_install_path/HiC-Pro_3.1.0/bin/HiC-Pro -i ${MAP_DATA}/merge_validpairs/hic_results/data -o ${MAP_DATA}/contact_maps -c ${MAP_DATA}/config-hicpro.txt -s build_contact_maps -s ice_norm -p
