6f0a6088a676bbd061bfa3af353ac7e547d16b63
Jingjing Wu <112022982+jingjingwu1998@users.noreply.github.com>
2025-01-29 13:08:44 -0500

    Delete HiChIP directory

--- HiChIP/1_HiCpro_hichipper_preprocessing.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# 1. Run HiC PRo
-HiC-Pro -i raw_fastq/ -o /HiC-Pro_out -c config-hicpro.txt
-
-# 2. Run HiChipper
-hichipper --out EBNA3A_H3K27ac_hichipper_merged  --make-ucsc  yaml/merged.yaml 
