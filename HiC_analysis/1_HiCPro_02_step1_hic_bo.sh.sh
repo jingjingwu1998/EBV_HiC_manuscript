9985b2e4c76310dd20517ebbfe58d099a221a01d
Jingjing Wu <112022982+jingjingwu1998@users.noreply.github.com>
2025-01-31 20:05:26 -0500

    Delete HiC_analysis/1_HiCPro_02_step1_hic_bo.sh.sh

--- HiC_analysis/1_HiCPro_02_step1_hic_bo.sh.sh
+++
@@ -1,3 +0,0 @@
-FASTQFILE=$SLURM_SUBMIT_DIR/inputfiles_hic_bo.txt; export FASTQFILE
-make --file /share/lab_teng/trainee/JingjingWu/EBV/HiC_performance/HiC-Pro_install_path/HiC-Pro_3.1.0/scripts/Makefile CONFIG_FILE=/share/lab_teng/trainee/JingjingWu/EBV/hicpro_mapping_2/0_config-hicpro.txt CONFIG_SYS=/share/lab_teng/trainee/JingjingWu/EBV/HiC_performance/HiC-Pro_install_path/config-system.txt mapping  2>&1
-~
